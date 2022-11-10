6b1ddcb69e1d893ab5476cc9fd0544cdd64dbbfa
nikoladubica <45574682+nikoladubica@users.noreply.github.com>
2022-11-10 15:18:55 +0100

    Limit to 8 items per page and category filter add

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,26 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Radio, Space, Select, Input, Button, Row, Col, Divider } from 'antd';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../store';
+import { RootState } from '../store/reducers';
+
 const Search: React.FC<{}> = () => {
+    const dispatch = useDispatch();
+    const state = useSelector((state: RootState) => state)
+
+    const { setFilters, filterData } = bindActionCreators(actionCreators, dispatch)
+
     const { Option, OptGroup } = Select;
-    const [value, setValue] = useState(1);
-
-    const onChange = (e: any) => {
-        setValue(e.target.value);
-    };
-
-    const handleMenuClick = (e: any) => {
-        console.log('click', e);
-    };
    
-    const handleChange = (e: any) => {
-        console.log('click', e);
+    const categoryChangeHandler = (e: any) => {
+        setFilters({key: 'category', value: e})
     };
+
+    const locationChangeHandler = (e: any) => {
+        setFilters({key: 'location', value: e})
+    };
+
+    useEffect(() => {
+        // setFilters({key: 'category', value: 'houses'})
+    }, [])
+
+    useEffect(() => {
+        filterData(Object.assign(state.original), state.filters, 1)
+    }, [state.filters])
 
     return (
         <>
             <div className="search section">
-                <Radio.Group onChange={onChange} value={value}>
+                <Radio.Group>
                     <Space direction="vertical">
                         <Radio value={1}>Kupujem</Radio>
                         <Radio value={2}>Iznajmljujem</Radio>
@@ -30,55 +43,47 @@ const Search: React.FC<{}> = () => {
                 <Row gutter={[20, 20]}>
                     <Col span={8}>
                         <Select
-                            defaultValue='home'
-                            onChange={handleChange}
+                            defaultValue='Tip nekretnine'
+                            onChange={categoryChangeHandler}
                             options={[
                                 {
-                                    value: 'home',
+                                    value: 'houses',
                                     label: 'Kuća',
                                 },
                                 {
-                                    value: 'apartment',
+                                    value: 'flats',
                                     label: 'Stan',
-                                },
-                                {
-                                    value: 'parcel',
-                                    label: 'Plac',
-                                },
-                                {
-                                    value: 'land',
-                                    label: 'Zemlja',
                                 },
                             ]}
                         />
                     </Col>
 
                     <Col span={8}>
-                        <Select placeholder='Lokacija'>
+                        <Select placeholder='Lokacija' onChange={}>
                             <Option value="">-</Option>
                             <OptGroup label="Centralna Hrvatska">
-                                <Option value="1">Zagreb</Option>
-                                <Option value="2">Karlovac</Option>
-                                <Option value="3">Bjelovar</Option>
-                                <Option value="4">Rijeka</Option>
-                                <Option value="5">Čakovec</Option>
-                                <Option value="6">Varaždin</Option>
+                                <Option value="Zagreb">Zagreb</Option>
+                                <Option value="Karlovac">Karlovac</Option>
+                                <Option value="Bjelovar">Bjelovar</Option>
+                                <Option value="Rijeka">Rijeka</Option>
+                                <Option value="Čakovec">Čakovec</Option>
+                                <Option value="Varaždin">Varaždin</Option>
                             </OptGroup>
                             <OptGroup label="Dalmacija">
-                                <Option value="7">Zadar</Option>
-                                <Option value="8">Šibenik</Option>
-                                <Option value="9">Split</Option>
-                                <Option value="10">Dubrovnik</Option>
+                                <Option value="Zadar">Zadar</Option>
+                                <Option value="Šibenik">Šibenik</Option>
+                                <Option value="Split">Split</Option>
+                                <Option value="Dubrovnik">Dubrovnik</Option>
                             </OptGroup>
                             <OptGroup label="Slavonija">
-                                <Option value="11">Osijek</Option>
-                                <Option value="12">Slavonski Brod</Option>
-                                <Option value="13">Vukovar</Option>
+                                <Option value="Osijek">Osijek</Option>
+                                <Option value="Slavonski Brod">Slavonski Brod</Option>
+                                <Option value="Vukovar">Vukovar</Option>
                             </OptGroup>
                             <OptGroup label="Istra">
-                                <Option value="14">Poreč</Option>
-                                <Option value="15">Pula</Option>
-                                <Option value="16">Rovinj</Option>
+                                <Option value="Poreč">Poreč</Option>
+                                <Option value="Pula">Pula</Option>
+                                <Option value="Rovinj">Rovinj</Option>
                             </OptGroup>
                         </Select>
                     </Col>
